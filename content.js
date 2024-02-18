19aaac83e834e97992b7a302d1ac412daefbbf18
AryanKumar2002 <76984197+AryanKumar2002@users.noreply.github.com>
2024-02-18 15:04:26 -0800

    Delete content.js

--- content.js
+++
@@ -1,6 +0,0 @@
-// content.js
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === 'readText' && request.text) {
-      chrome.tts.speak(request.text);
-    }
-  });
