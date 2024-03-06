e098c4ca7b0143530f26aee38424c41a7ca1e274
AryanKumar2002 <76984197+AryanKumar2002@users.noreply.github.com>
2024-03-06 15:24:25 -0800

    Delete Menu Addition directory

--- Menu Addition/popup.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('speak').addEventListener('click', function() {
-    var text = document.getElementById('text-to-speak').value;
-    chrome.tts.speak(text, {'lang': 'en-US', 'rate': 1.0});
-  });
-  
