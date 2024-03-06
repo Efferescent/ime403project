fa7a8d9c2595a80ec3162d76653fd6a8f8ab7ddd
AryanKumar2002 <akumar54@calpoly.edu>
2024-03-06 15:18:34 -0800

    merged

--- popup.js
+++ popup.js
@@ -1,5 +1,32 @@
-document.getElementById('speak').addEventListener('click', function() {
+var speechUtterance = null;
+
+document.getElementById('speak').addEventListener('click', function () {
     var text = document.getElementById('text-to-speak').value;
-    chrome.tts.speak(text, {'lang': 'en-US', 'rate': 1.0});
-  });
-  
+    var rate = parseFloat(document.getElementById('speed').value);
+    speakText(text, rate);
+});
+
+document.getElementById('pause').addEventListener('click', function () {
+    if (speechUtterance) {
+        if (speechUtterance.paused) {
+            chrome.tts.resume();
+        } else {
+            chrome.tts.pause();
+        }
+    }
+});
+
+function speakText(text, rate) {
+    if (speechUtterance) {
+        chrome.tts.stop();
+    }
+
+    speechUtterance = new SpeechSynthesisUtterance(text);
+    speechUtterance.lang = 'en-US';
+    speechUtterance.rate = rate;
+
+    chrome.tts.speak(text, {
+        'lang': 'en-US',
+        'rate': rate
+    });
+}
